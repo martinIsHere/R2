8284f7b90465e2ade3ab7133e7d273c87fdce681
martinIsHere <martin.havardsen@gmail.com>
2025-10-14 12:18:39 +0200

    first commit; main pc

---
+++ 1_1.py
@@ -0,0 +1,16 @@
+"""
+"""
+
+class test1:
+    def __init__(self):
+        pass
+
+a = int(input("a1: "))
+n = int(input("mengde ledd: "))
+
+
+for i in range(n):
+    print(a)
+    a+=1
+
+t = test1()
